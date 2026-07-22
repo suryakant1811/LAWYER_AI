d5c207c8e9b77cb2ef85c2e720af6a0fd426a66b
suryadwd <surajdwivedi644@gmail.com>
2026-07-22 10:25:07 +0530

    added planner nodes

--- main.py
+++ main.py
@@ -28,4 +28,4 @@ result2 = workflow2.invoke(
     }
 )
 
-print(result2)
+print(result2["action_plan"])
--- workflow2.py
+++ workflow2.py
@@ -1,19 +1,20 @@
 from langgraph.graph import START, END, StateGraph
 from nodes.router_node import get_router_node
 from state.state import ConversationState
-from nodes.question_node import get_question_node
 from nodes.retriever_node import get_retriever_node
 from nodes.reasoning_node import get_reasoning_node
-
+from nodes.planner_node import get_planner_node
 graph = StateGraph(ConversationState)
 
 graph.add_node("Router", get_router_node)
 graph.add_node("Retriever", get_retriever_node)
 graph.add_node("Reasoning", get_reasoning_node)
+graph.add_node("Planner", get_planner_node)
 
 graph.add_edge(START, "Router")
 graph.add_edge("Router", "Retriever")
 graph.add_edge("Retriever", "Reasoning")
-graph.add_edge("Reasoning", END)
+graph.add_edge("Reasoning", "Planner")
+graph.add_edge("Planner", END)
 
 workflow2 = graph.compile()
